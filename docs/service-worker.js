3ad4c8b6ca0c8f1c11d30de8ee2b0b67ff687c21
orelcain <orelcain@hotmail.com>
2025-12-09 11:38:24 -0300

    v6.017: Removido indicadores de carga (WarmupProgress/LoadingIndicator) - Firebase carga instantaneamente

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -5,7 +5,7 @@
  * v6.012 - Fix variables duplicadas
  */
 
-const CACHE_NAME = 'inventario-v6.012';
+const CACHE_NAME = 'inventario-v6.017';
 const DYNAMIC_CACHE = 'inventario-dynamic-v6.012';
 
 // Archivos esenciales para funcionar offline
