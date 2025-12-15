a364ee954d130266001074bda37133352ffb700e
orelcain <orelcain@hotmail.com>
2025-12-15 18:43:17 -0300

    v6.133b - Actualizar versión y cache

--- v6.0/service-worker.js
+++ v6.0/service-worker.js
@@ -10,8 +10,8 @@
  * v6.131 - Forms -15%, Wizard compacto, Toasts -20%, iPhone Pro optimizado
  */
 
-const CACHE_NAME = 'inventario-v6.133';
-const DYNAMIC_CACHE = 'inventario-dynamic-v6.133';
+const CACHE_NAME = 'inventario-v6.133b';
+const DYNAMIC_CACHE = 'inventario-dynamic-v6.133b';
 
 // Archivos esenciales para funcionar offline
 const STATIC_ASSETS = [
